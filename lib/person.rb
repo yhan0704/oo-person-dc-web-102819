601fdc78da5724f1290c61318e0bcc23828cedfe
Young Han <han20@umbc.edu>
2019-10-30 21:45:37 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,21 @@
-# your code goes here
+class Person
+  attr_accessor :name, :account, :happiness, :hygiene
+  @@bank_account = 0 
+  def initialize(name, account = 25, happiness = 8, hygiene = 8)
+    @name = name
+    @account = account
+    @happiness = happiness
+    @hygiene = hygiene
+    @@bank_account = 0  
+  end
+
+  def happy?
+    if @happiness > 7 && @happiness<=10
+      return true
+    else 
+      return false
+  end
+end
+  
+end
+
